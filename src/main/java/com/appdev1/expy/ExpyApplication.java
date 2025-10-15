19a95a7eec363f0b90e72ce4397f76657a98858c
Luis <chrisluisfern1@gmail.com>
2025-10-15 10:48:21 +0800

    Initial commit

---
+++ src/main/java/com/appdev1/expy/ExpyApplication.java
@@ -0,0 +1,13 @@
+package com.appdev1.expy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExpyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExpyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/appdev1/expy/ExpyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.appdev1.expy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExpyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
